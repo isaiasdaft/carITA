2fc8efd8e0b7022b390379488bb3f7277aa79ba4
unknown <guez.emilio92@gmail.com>
2023-04-18 09:28:53 -0600

    Actualice app sobre el Login

--- App.js
+++ App.js
@@ -1,23 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import * as React from "react";
-import { Box, Text, Heading, VStack, FormControl, Input, Link, Button, HStack, Center, NativeBaseProvider } from "native-base";
+import React from "react";
+import {
+  Text,
+  Link,
+  HStack,
+  Center,
+  Heading,
+  Switch,
+  useColorMode,
+  NativeBaseProvider,
+  extendTheme,
+  VStack,
+  Box,
+} from "native-base";
+import NativeBaseIcon from "./components/NativeBaseIcon";
+import { Platform } from "react-native";
+import Login from "./src/screens/Login";
+
+// Define the config
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+};
+
+// extend the theme
+export const theme = extendTheme({ config });
 
 export default function App() {
   return (
     <NativeBaseProvider>
-           <Center flex={1} px="3">
-                <Example />
+             <Center flex={1} px="3">
+                <Login />
             </Center>
-    </NativeBaseProvider>
+         </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
