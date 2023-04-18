96fe3a1aebbd1e529f2fdb80bffe8ab99ee5a24c
unknown <guez.emilio92@gmail.com>
2023-04-17 21:20:16 -0600

    Agregue Login

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Box, Text, Heading, VStack, FormControl, Input, Link, Button, HStack, Center, NativeBaseProvider } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+           <Center flex={1} px="3">
+                <Example />
+            </Center>
+    </NativeBaseProvider>
   );
 }
 
