ecf144fccf4e172edc56f6183ff68615e780e3bb
JesusAnd <jesusandradesustaita@gmail.com>
2023-05-08 19:14:26 -0600

    Update Historial.js

--- src/screens/Historial.js
+++ src/screens/Historial.js
@@ -1,16 +1,38 @@
 import React from "react";
-import {Text, Checkbox, Center, NativeBaseProvider } from "native-base";
+import { Text, Checkbox, Center, NativeBaseProvider } from "native-base";
 
 const Historial = () => {
   const [groupValues, setGroupValues] = React.useState([]);
-  return <Checkbox.Group onChange={setGroupValues} value={groupValues} accessibilityLabel="choose numbers">
-      <Text>Trabajos completos</Text>
-      <Checkbox value="one" my={2}>
-        Alex - Nissan
-      </Checkbox>
-      <Checkbox value="two">Andrea - Versa</Checkbox>
-      <Checkbox value="tree">Leonor - Chevy</Checkbox>
-    </Checkbox.Group>;
+
+  return (
+    <NativeBaseProvider>
+      <Center>
+        <Text fontSize="xl" fontWeight="bold" mb={4}>Trabajos completos</Text>
+        <Checkbox.Group onChange={setGroupValues} value={groupValues} accessibilityLabel="choose numbers">
+          <Checkbox value="one" my={2}>
+            <Text>
+              Alex - Nissan{"\n"}
+              <Text fontSize="sm" color="gray.500">Fecha: 09/05/2023</Text>
+            </Text>
+          </Checkbox>
+          <Checkbox value="two" my={2}>
+            <Text>
+              Andrea - Versa{"\n"}
+              <Text fontSize="sm" color="gray.500">Fecha: 17/04/2023</Text>
+            </Text>
+          </Checkbox>
+          <Checkbox value="tree" my={2}>
+            <Text>
+              Leonor - Chevy{"\n"}
+              <Text fontSize="sm" color="gray.500">Fecha: 23/03/2023</Text>
+            </Text>
+          </Checkbox>
+        </Checkbox.Group>
+      </Center>
+    </NativeBaseProvider>
+  );
 };
 
 export default Historial;
+
+
